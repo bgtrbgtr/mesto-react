9e0d9810ed21f888aa59e9e635d3d24a4b741eb1
bgtrbgtr <jhmwmw@gmail.com>
2022-09-02 13:00:54 +0200

    Updates

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import logoPath from "../images/logo.svg";
-
-export default function Header() {
-  return (
-    <header className="header">
-      <img src={logoPath} alt="Логотип Mesto" className="header__logo" />
-    </header>
-  );
-}
